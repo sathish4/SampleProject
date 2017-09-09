249d551742448783439f2088fbc765450ecab593
sathish4 <reddy.sathish0001@gmail.com>
2017-09-09 11:55:30 -0500

    Added drivers and maven test

--- src/test/java/SeleniumAutomation/Git/AppTest.java
+++ src/test/java/SeleniumAutomation/Git/AppTest.java
@@ -1,11 +1,44 @@
 package SeleniumAutomation.Git;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
 /**
  * Unit test for simple App.
  */
 public class AppTest {
 	
-	public static void main(String[] args){
-		System.out.println("Hello");
+	WebDriver driver;
+	
+	@BeforeClass
+	public void setup() {
+		
+		System.setProperty("webdriver.gecko.driver", "E:\\Git\\Drivers\\geckodriver.exe");
+		driver = new FirefoxDriver();
+		driver.get("http://www.google.com/");
+		driver.manage().window().maximize();
+
 	}
+	@Test
+	public void verifyLogin(){
+		
+		WebElement ele = driver.findElement(By.name("q"));
+		ele.sendKeys("Hello");
+		String actualTitle = driver.getTitle();
+		String expectedTitle = "Google";
+		Assert.assertEquals(actualTitle, expectedTitle);
+	}
+	
+	@AfterClass
+	public void tearDown(){
+		
+		driver.quit();
+	}
+	
 }
